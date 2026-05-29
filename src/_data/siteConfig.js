613386c2bc0ad2db1920237294d10e34b3ba846b
Fxanadu <deepwhite86@outlook.com>
2026-05-29 16:05:46 +0800

    chore: 更新个人配置信息

--- src/_data/siteConfig.js
+++ src/_data/siteConfig.js
@@ -3,16 +3,16 @@ module.exports = {
   description: "DJY 静态站点 - 基于 Eleventy 构建",
   url: process.env.SITE_URL || "",
   language: "zh-CN",
-  about: "一个喜欢技术、咖啡和旅行的人。用代码构建有趣的东西，用文字记录生活的点滴。",
+  about: "一个热爱电影、音乐和生活的人。",
   footer: {
     copyright: "DJY",
     poweredBy: "基于 Eleventy 构建"
   },
   contacts: [
-    { name: "GitHub", value: "dengjunyu", url: "https://github.com/dengjunyu" },
+    { name: "GitHub", value: "FXnadu", url: "https://github.com/FXnadu" },
     { name: "Blog", value: "deepwhitex.com", url: "https://deepwhitex.com" },
-    { name: "Email", value: "hi@dengjunyu.com", url: "mailto:hi@dengjunyu.com" },
+    { name: "Email", value: "deepwhitex@outlook.com", url: "mailto:deepwhitex@outlook.com" },
     { name: "微信", value: "dengjunyu_wx" },
-    { name: "QQ", value: "123456789" },
+    { name: "QQ", value: "2035083310" },
   ]
 };
